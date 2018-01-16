3414554a5d6b3001c65d8b69d6b96805019b6ceb
amdonov <donovan_aaron@bah.com>
2018-01-16 16:01:10 -0500

    Changed to Exclusive XML Canonicalization in Algorithm rather than Canonical XML so that signatures could be used with WS-Security. Should not impact golang implementation because namespaces are declared on every element.

--- xmlsig.go
+++ xmlsig.go
@@ -100,7 +100,8 @@ func (s *signer) Sign(data []byte) (string, error) {
 
 func newSignature() *Signature {
 	signature := &Signature{}
-	signature.SignedInfo.CanonicalizationMethod.Algorithm = "http://www.w3.org/TR/2001/REC-xml-c14n-20010315"
+	signature.SignedInfo.CanonicalizationMethod.Algorithm =
+		"http://www.w3.org/2001/10/xml-exc-c14n#"
 	transforms := &signature.SignedInfo.Reference.Transforms.Transform
 	*transforms = append(*transforms, Algorithm{"http://www.w3.org/2000/09/xmldsig#enveloped-signature"})
 	*transforms = append(*transforms, Algorithm{"http://www.w3.org/2001/10/xml-exc-c14n#"})
